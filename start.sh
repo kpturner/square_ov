cce302968c9a8c1d3a5e8a64b74f6d4363c00b0f
Kevin <kevin@kpturner.co.uk>
2025-10-27 12:12:42 +0000

    fix: db password

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-export DB_PASSWORD="${DB_PASSWORD_SECRET}_se-square-ov"
+export DB_PASSWORD="${DB_PASSWORD_SECRET}_square-ov"
 export DATABASE_URL="mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # The DB updates will not work on a container restart because we are about to remove the secrets but 
 # we only need to work once per deployment
 rm -rf ./config/secrets
+
+echo "DATABASE_URL is $DATABASE_URL"
 
 # printenv && echo 'Ready!' && tail -f /dev/null
 yarn prisma migrate deploy
