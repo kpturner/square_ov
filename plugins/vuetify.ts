5c74e991b17a227c9a1911ca80942ed27a9cedc2
Kevin <kevin@kpturner.co.uk>
2025-10-27 11:45:03 +0000

    fix: build error

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,15 +4,9 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 // import * as components from 'vuetify/components';
 // import * as directives from 'vuetify/directives';
-import { VNumberInput } from 'vuetify/labs/VNumberInput';
-import { VDateInput } from 'vuetify/labs/VDateInput';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    components: {
-      VNumberInput,
-      VDateInput,
-    },
     //directives,
   });
   app.vueApp.use(vuetify);
