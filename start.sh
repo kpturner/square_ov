4f270b4a3fbe08514740a8ff23a81d2be40fdb41
Kevin <kevin@kpturner.co.uk>
2025-10-27 12:28:46 +0000

    chore: more diagnostics

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ export DATABASE_URL="mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${D
 
 # The DB updates will not work on a container restart because we are about to remove the secrets but 
 # we only need to work once per deployment
+cat ./config/secrets
 rm -rf ./config/secrets
 
 echo "DATABASE_URL is $DATABASE_URL"
 
-# printenv && echo 'Ready!' && tail -f /dev/null
+printenv && echo 'Ready!' && tail -f /dev/null
 yarn prisma migrate deploy
 
 node .output/server/index.mjs "$@"
