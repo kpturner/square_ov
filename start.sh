4890abd7a1428ad974c192d28d7bc8038cf5a361
Kevin <kevin@kpturner.co.uk>
2025-10-27 12:43:29 +0000

    fix: startup script

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+DB_PASSWORD_SECRET=$(cat ./config/secrets/db_password_secret.txt)
 export DB_PASSWORD="${DB_PASSWORD_SECRET}_square-ov"
 export DATABASE_URL="mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # The DB updates will not work on a container restart because we are about to remove the secrets but 
 # we only need to work once per deployment
-cat ./config/secrets
 rm -rf ./config/secrets
 
 echo "DATABASE_URL is $DATABASE_URL"
 
-printenv && echo 'Ready!' && tail -f /dev/null
+# printenv && echo 'Ready!' && tail -f /dev/null
 yarn prisma migrate deploy
 
 node .output/server/index.mjs "$@"
