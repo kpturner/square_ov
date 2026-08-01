c7b9832f3df1591df8279d40272004021b08841c
Kevin <kevin@kpturner.co.uk>
2026-08-01 12:36:23 +0100

    fix: vip rank constants again

--- utils/constants.ts
+++ utils/constants.ts
@@ -32,4 +32,4 @@ export function ovTypeLabel(type: 'craft' | 'ra') {
   return OV_TYPE_LABELS[type];
 }
 
-export const VIP_RANKS = ['PGM', 'DPGM', 'APGM', 'MEGS', 'DGSUPT', '2NDPGP', '3RDPGP', 'GSN'];
+export const VIP_RANKS = ['PGM', 'DPGM', 'APGM', 'MEGS', 'DGSUPT', '2NDPGP', '3RDPGP'];
